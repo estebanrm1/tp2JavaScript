b57fe4bb1577f66533d952fedd39194f61e2874e
estebanrm1 <solsel90@gmail.com>
2023-03-08 20:31:30 -0300

    se resolvio el ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,6 +1,11 @@
-const edad = parseInt(prompt('Ingrese su edad'));
+const edad = parseInt(prompt("Ingrese su edad"));
 
-if(edad > 18) {
-    document.write('Usted ya puede conducir');
-
+if (edad > 10 && edad <= 80) {
+    if (edad > 18) {
+        document.write("Usted ya puede conducir");
+    } else {
+        document.write("Usted no puede conducir");
+    }
+} else {
+    document.write("Ingreso un valor invalido");
 }
