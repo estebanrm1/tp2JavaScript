8d733680f2843b87855193540bb74a5638ea94a1
estebanrm1 <solsel90@gmail.com>
2023-03-12 21:40:32 -0300

    se resolvio el ejercicio 7

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -4,5 +4,5 @@ for (numero = 1; numero <= 30; numero++) {
     for (repeticiones = 0; repeticiones < numero; repeticiones++) {
         document.write(numero);
     }
-    document.write("<br>");
+    document.write(`<br>`);
 }
---
+++ js/ejercicio7.js
@@ -0,0 +1,17 @@
+let numRepe = parseInt(prompt('Ingrese el numero de repeticiones'));
+
+if(isNaN(numRepe) == false){
+    if(numRepe > 0 && numRepe <=50){
+    let i,rep
+        for(i = numRepe; i >= 1; i--){
+            for(rep = i; rep >= 1; rep--){
+                document.write(i)
+            }
+            document.write(`<br>`)
+        }
+    }else{
+        alert('El numero ingresado no es valido');
+    }
+}else{
+    alert('Ingrese UN NUMERO');
+}
